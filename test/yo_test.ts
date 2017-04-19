ad3b76b8a7a66621a77c8416d978c568b216889f
Michael Raimondi <mike@mikeraimondi.com>
2017-04-18 23:31:57 -0400

    linted test

--- test/yo_test.ts
+++ test/yo_test.ts
@@ -1,25 +1,25 @@
-import * as mocha from 'mocha';
-import * as chai from 'chai';
-import chaiHttp = require('chai-http');
+import * as chai from "chai";
+import * as mocha from "mocha";
+import chaiHttp = require("chai-http");
 
-import app from '../src/App';
+import app from "../src/App";
 
 chai.use(chaiHttp);
 const expect = chai.expect;
 
-describe('baseRoute', () => {
+describe("baseRoute", () => {
 
-  it('should be json', () => {
-    return chai.request(app.express).get('/')
-    .then(res => {
-      expect(res.type).to.eql('application/json');
+  it("should be json", () => {
+    return chai.request(app.express).get("/")
+    .then( (res) => {
+      expect(res.type).to.eql("application/json");
     });
   });
 
-  it('should have a message prop', () => {
-    return chai.request(app.express).get('/')
-    .then(res => {
-      expect(res.body.message).to.eql('yo');
+  it("should have a message prop", () => {
+    return chai.request(app.express).get("/")
+    .then( (res) => {
+      expect(res.body.message).to.eql("yo");
     });
   });
 
